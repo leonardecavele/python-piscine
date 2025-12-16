dedf0dc5c12f579330234d34e359152f95d5ad05
Leonard Decavele <ldecavel@z4r2p5.42lyon.fr>
2025-12-16 11:51:03 +0100

    fix: recursive

--- module00/ex5/ft_count_harvest_recursive.py
+++ module00/ex5/ft_count_harvest_recursive.py
@@ -10,14 +10,14 @@
 #                                                                              #
 # **************************************************************************** #
 
-def ft_count_harvest_recursive() -> None:
-    days: int = None
+def ft_count_harvest_recursive(day: int = None, days: int = None) -> None:
     if not days:
-        day: int = 0
         days = int(input("Days until harvest: "))
+    if not day:
+        day = 0
     day += 1
     print(f"Day {day}")
     if day == days:
         print("Harvest time!")
     else:
-        ft_count_harvest_recursive()
+        ft_count_harvest_recursive(day, days)
