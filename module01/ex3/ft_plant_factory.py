1b1dca0b79913fc0f02d182b12317424fe454f6a
Leonard Decavele <ldecavel@z2r12p5.42lyon.fr>
2025-12-17 15:57:17 +0100

    edit: is a factory

--- module01/ex3/ft_plant_factory.py
+++ module01/ex3/ft_plant_factory.py
@@ -6,16 +6,26 @@ class Plant:
         self.height = height
         self.age = age
         Plant.count += 1
-        print(f"Created: {name} ({height}cm, {age} days)")
+
+
+class Garden:
+
+    def __init__(self) -> None:
+        self.plants = []
+
+    def add(self, new: Plant) -> None:
+        self.plants += [Plant]
+        print(f"Created: {new.name} ({new.height}cm, {new.age} days)")
 
 
 def main() -> None:
+    g1 = Garden()
     print("=== Plant Factory Output ===")
-    Plant("Rose", 25, 30)
-    Plant("Oak", 200, 365)
-    Plant("Cactus", 5, 90)
-    Plant("Sunflower", 80, 45)
-    Plant("Fern", 15, 120)
+    g1.add(Plant("Rose", 25, 30))
+    g1.add(Plant("Oak", 200, 365))
+    g1.add(Plant("Cactus", 5, 90))
+    g1.add(Plant("Sunflower", 80, 45))
+    g1.add(Plant("Fern", 15, 120))
     print(f"\nTotal plants created: {Plant.count}")
 
 
