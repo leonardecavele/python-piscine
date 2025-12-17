da7eca4e4845d7ae64c3533be1b8308925780627
Leonard Decavele <ldecavel@z2r12p5.42lyon.fr>
2025-12-17 14:19:13 +0100

    edit: added main

--- module01/ex4/ft_garden_security.py
+++ module01/ex4/ft_garden_security.py
@@ -2,7 +2,9 @@ class SecurePlant:
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         print(f"Plant created: {self.name}")
+        self.height = 0
         self.set_height(height)
+        self.age = 0
         self.set_age(age)
 
     def set_height(self, height: int):
@@ -22,4 +24,17 @@ class SecurePlant:
             print(f"Age updated: {age} days [OK]")
 
     def get_info(self):
-        print(f"Current plant: {self.name} {self.height}cm, {self.age} days)")
+        print(f"Current plant: {self.name} ({self.height}cm, {self.age} days)")
+
+
+def main() -> None:
+    print("=== Garden Security System ===")
+    p1 = SecurePlant("Rose", 25, 30)
+    print()
+    p1.set_height(-5)
+    print()
+    p1.get_info()
+
+
+if __name__ == "__main__":
+    main()
