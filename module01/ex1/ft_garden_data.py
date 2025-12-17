da428f66e04a6456c1f38a468e870df10338a481
Leonard Decavele <ldecavel@z2r12p5.42lyon.fr>
2025-12-17 11:27:54 +0100

    module01 ex1

--- module01/ex1/ft_garden_data.py
+++ module01/ex1/ft_garden_data.py
@@ -1,5 +1,21 @@
-class plant:
+class Plant:
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.height = height
         self.age = age
+
+    def __str__(self) -> None:
+        return f"{self.name}: {self.height}cm, {self.age} days old"
+
+
+def main() -> None:
+    p1 = Plant("Rose", 25, 30)
+    p2 = Plant("Sunflower", 80, 45)
+    p3 = Plant("Cactus", 15, 120)
+    print(p1)
+    print(p2)
+    print(p3)
+
+
+if __name__ == "__main__":
+    main()
