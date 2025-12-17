1facc251e31d845f5cec2ce5d2898ba603ad8d70
Leonard Decavele <ldecavel@z2r12p5.42lyon.fr>
2025-12-17 15:58:32 +0100

    fix: __ instead of _ to properly protect variables

--- module01/ex4/ft_garden_security.py
+++ module01/ex4/ft_garden_security.py
@@ -2,43 +2,43 @@ class SecurePlant:
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         print(f"Plant created: {self.name}")
-        self._height = 0
-        self.set_height(height)
-        self._age = 0
-        self.set_age(age)
+        self.__height = 0
+        self.set__height(height)
+        self.__age = 0
+        self.set__age(age)
 
-    def set_height(self, height: int) -> None:
+    def set__height(self, height: int) -> None:
         if height < 0:
             print(f"Invalid operation attempted: height {height}cm [REJECTED]")
             print("Security: Negative height rejected")
         else:
-            self._height = height
+            self.__height = height
             print(f"Height updated: {height}cm [OK]")
 
-    def set_age(self, age: int) -> None:
+    def set__age(self, age: int) -> None:
         if age < 0:
             print(f"Invalid operation attempted: age {age} days [REJECTED]")
             print("Security: Negative age rejected")
         else:
-            self._age = age
+            self.__age = age
             print(f"Age updated: {age} days [OK]")
 
-    def get_height(self) -> int:
-        return self._height
+    def get__height(self) -> int:
+        return self.__height
 
-    def get_age(self) -> int:
-        return self._age
+    def get__age(self) -> int:
+        return self.__age
 
     def get_info(self) -> None:
         print(f"Current plant: {self.name} ", end='')
-        print(f"({self._height}cm, {self._age} days)")
+        print(f"({self.__height}cm, {self.__age} days)")
 
 
 def main() -> None:
     print("=== Garden Security System ===")
     p1 = SecurePlant("Rose", 25, 30)
     print()
-    p1.set_height(-5)
+    p1.set__height(-5)
     print()
     p1.get_info()
 
