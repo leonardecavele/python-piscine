6ae626d0d4d7dae0f1d362504a100c02e07ee454
Leonard Decavele <ldecavel@z2r12p5.42lyon.fr>
2025-12-17 14:45:49 +0100

    fix: encapsulation

--- module01/ex4/ft_garden_security.py
+++ module01/ex4/ft_garden_security.py
@@ -2,29 +2,36 @@ class SecurePlant:
     def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         print(f"Plant created: {self.name}")
-        self.height = 0
+        self._height = 0
         self.set_height(height)
-        self.age = 0
+        self._age = 0
         self.set_age(age)
 
-    def set_height(self, height: int):
+    def set_height(self, height: int) -> None:
         if height < 0:
             print(f"Invalid operation attempted: height {height}cm [REJECTED]")
             print("Security: Negative height rejected")
         else:
-            self.height = height
+            self._height = height
             print(f"Height updated: {height}cm [OK]")
 
-    def set_age(self, age: int):
+    def set_age(self, age: int) -> None:
         if age < 0:
             print(f"Invalid operation attempted: age {age} days [REJECTED]")
             print("Security: Negative age rejected")
         else:
-            self.age = age
+            self._age = age
             print(f"Age updated: {age} days [OK]")
 
-    def get_info(self):
-        print(f"Current plant: {self.name} ({self.height}cm, {self.age} days)")
+    def get_height(self) -> int:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
+
+    def get_info(self) -> None:
+        print(f"Current plant: {self.name} ", end='')
+        print(f"({self._height}cm, {self._age} days)")
 
 
 def main() -> None:
