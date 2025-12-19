804ed35b27d7edb215d86604336d2f68bf2bddad
leonardecavele <leonard.decavele@gmail.com>
2025-12-19 04:57:22 +0100

    fix: printing errors

--- module00/ex4/ft_water_reminder.py
+++ module00/ex4/ft_water_reminder.py
@@ -2,4 +2,4 @@ def ft_water_reminder() -> None:
     if int(input("Days since last watering: ")) > 2:
         print("Water the plants!")
     else:
-        print("Plants are fine.")
+        print("Plants are fine")
--- module00/ex7/ft_seed_inventory.py
+++ module00/ex7/ft_seed_inventory.py
@@ -1,11 +1,9 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
-    if unit == "packets" or unit == "grams" or unit == "area":
-        print(f"{seed_type.capitalize()} seeds: {quantity} ", end="")
-        if unit == "packets":
-            print("available")
-        elif unit == "grams":
-            print("total")
-        elif unit == "area":
-            print("square meters")
+    if unit == "packets":
+        print(f"{seed_type.capitalize()} seeds: {quantity} packets available")
+    elif unit == "grams":
+        print(f"{seed_type.capitalize()} seeds: {quantity} grams total")
+    elif unit == "area":
+        print(f"{seed_type.capitalize()} seeds: covers {quantity} square meters")
     else:
-        print("Unknown unit type.")
+        print("Unknown unit type")
